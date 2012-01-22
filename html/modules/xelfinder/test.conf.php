bb6cfe31fd3ff25a2591c818fc490e6d16ed5621
nao-pon <hypweb@gmail.com>
2012-01-22 14:00:57 +0000

    サンプル用ホルダ作成＆test_conf.php

--- html/modules/xelfinder/test.conf.php
+++ html/modules/xelfinder/test.conf.php
@@ -3,11 +3,15 @@
 // for test
 // ['volume_setting'] = 'mydirname:plugin:path:title:options, ...';
 
-$config['volume_setting'] = 'myalbum:myalbum:uploads/photos:イメージマネージャー, mailbbs:mailbbs:modules/mailbbs/imgs:写メールBBS, xelfinder:xelfinder:uploads/elfinder:共有ホルダ';
+$config['volume_setting'] =
+'myalbum:myalbum:uploads/photos:イメージマネージャー
+mailbbs:mailbbs:modules/mailbbs/imgs:写メールBBS
+xelfinder:xelfinder:uploads/elfinder:共有ホルダ';
 
 
 // xelfinder 共有ホルダのアクセス権限
 
+// 基本設定
 $attributes = array(
 //	array(
 //		'pattern' => '~\.php$~i',
@@ -16,25 +20,34 @@ $attributes = array(
 //		'hidden' => false,
 //		'locked' => true
 //	),
+	// .(dot) start
 	array(
 		'pattern' => '#/\.#',
 		'read' => false,
 		'write' => false,
 		'hidden' => true,
-		'locked' => false
+		'locked' => true
 	),
 );
 
-$attributes_guest = array(
+// メンバー用
+$attributes_member = array(
 	array(
-		'pattern' => '#/locked.*#',
+		'pattern' => '#^/forGuest\b#',
 		'read' => true,
 		'write' => true,
 		'hidden' => false,
 		'locked' => true
 	),
 	array(
-		'pattern' => '#/_.*#',
+		'pattern' => '#^/forMember\b#',
+		'read' => true,
+		'write' => true,
+		'hidden' => false,
+		'locked' => true
+	),
+	array(
+		'pattern' => '#.+#',
 		'read' => true,
 		'write' => false,
 		'hidden' => false,
@@ -42,18 +55,37 @@ $attributes_guest = array(
 	),
 );
 
-if (! $isAdmin) {
+// ゲスト用
+$attributes_guest = array(
+	array(
+		'pattern' => '#^/forGuest\b#',
+		'read' => true,
+		'write' => true,
+		'hidden' => false,
+		'locked' => true
+	),
+	array(
+		'pattern' => '#.+#',
+		'read' => true,
+		'write' => false,
+		'hidden' => false,
+		'locked' => true
+	),
+);
+// ユーザーランクで組み立て
+if (! $memberUid) {
 	$attributes = array_merge($attributes, $attributes_guest);
+} else if (! $isAdmin) {
+	$attributes = array_merge($attributes, $attributes_member);
 }
 
+// $extras に登録
 // $extras[key:mydirename] = (extra options array);
 $extras = array(
-	'xelfinder' => array(
+	$mydirname => array(
 		'attributes' => $attributes
 	)
 );
 
 // end for test
 ////////////////////////////
-
-
