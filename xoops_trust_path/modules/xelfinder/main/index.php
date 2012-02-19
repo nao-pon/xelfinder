a08ec833ae21b274e59a5358ab5787d8ac4d3c1b
nao-pon <hypweb@gmail.com>
2012-02-19 23:23:15 +0900

    Open elFinder window on module top page.

--- xoops_trust_path/modules/xelfinder/main/index.php
+++ xoops_trust_path/modules/xelfinder/main/index.php
@@ -4,8 +4,29 @@
  * $Id: index.php,v 1.1 2012/01/22 20:26:49 nao-pon Exp $
  */
 
+$xelfinderOpenJs = 'openWithSelfMain("'.XOOPS_URL.'/modules/'.$mydirname.'/manager.php", "elfinder", 750, 500);';
+
 include XOOPS_ROOT_PATH.'/header.php' ;
 
-echo 'There is nothing.';
+echo '<h4>xelFinder</h4>';
+
+echo '<script>
+	(function(){
+		var windowonload;
+		if (typeof window.onload == "function") {
+			windowonload = window.onload();
+		}
+		window.onload=function(){
+			if (windowonload) {
+				windowonload();
+			}
+			'.$xelfinderOpenJs.'
+		};
+	}());
+</script>';
+
+echo '<a href="#" onclick="'.htmlspecialchars($xelfinderOpenJs).'return false;">Open elFinder window</span>';
 
 include XOOPS_ROOT_PATH.'/footer.php';
+
+//include '../manager.php';
