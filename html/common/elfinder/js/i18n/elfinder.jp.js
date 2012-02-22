16c321e93c3b773e5de207ce5e8413d4af19a313
nao-pon <hypweb@gmail.com>
2012-02-22 09:04:19 +0900

    Merge from Studio-42:3f6c836d1b:2012-02-22

--- html/common/elfinder/js/i18n/elfinder.jp.js
+++ html/common/elfinder/js/i18n/elfinder.jp.js
@@ -1,7 +1,7 @@
 /**
  * Japanese translation
  * @author Tomoaki Yoshida <info@yoshida-studio.jp>
- * @version 2011-08-26
+ * @version 2012-02-22
  */
 if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object') {
 	elFinder.prototype.i18.jp = {
