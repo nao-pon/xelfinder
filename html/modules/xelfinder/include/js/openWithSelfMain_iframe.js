211e7f89dbd2ac1b9ee0f902679d3c112857b6df
nao-pon <hypweb@gmail.com>
2012-03-04 13:15:59 +0900

    Clean up & simplify.

--- html/modules/xelfinder/include/js/openWithSelfMain_iframe.js
+++ html/modules/xelfinder/include/js/openWithSelfMain_iframe.js
@@ -30,10 +30,11 @@ var XELFINDER_URL;
 })();
 
 function openWithSelfMain(url, name, w, h, returnwindow) {
-	//var purl = url.match(/^()\/$/);
-	h = jQuery(window).height() - 40;
-	w = jQuery(window).width() - 40;
-	jQuery.modal('<iframe name="'+name+'" id="xelf_window" src="' + url + '" height="'+h+'" width="'+w+'" style="border:0;overflow:hidden;" allowtransparency="true" scrolling="no">', {
+	var $ = jQuery;
+	
+	w = $(window).width() - 40;
+	h = $(window).height() - 40;
+	$.modal('<iframe name="'+name+'" id="xelf_window" src="' + url + '" height="'+h+'" width="'+w+'" style="border:0;overflow:hidden;" allowtransparency="true" scrolling="no" frameborder="0">', {
 		containerCss:{
 			backgroundColor:	"transparent",
 			borderColor:		"transparent",
@@ -51,81 +52,13 @@ function openWithSelfMain(url, name, w, h, returnwindow) {
 		overlayClose:		 true
 	});
 
-	var popW = document.getElementById('xelf_window');
-
-//	jQuery('#xelf_window').load(
-//		function($){
-//			$('#simplemodal-container').draggable();
-//			$('#simplemodal-container').resizable({
-//				handles: 'e, s',
-//				alsoResize: '#xelf_window',
-//				start: function(event, ui)
-//				{
-//				  // Add frame helpers
-//				  $("iframe").each(function() {
-//					var offsetWidth = this.offsetWidth, offsetHeight = this.offsetHeight;
-//
-//					$('<div class="ui-resizable-iframeFix" style="background: #fff;"></div>')
-//					  .css({
-//						width: offsetWidth+"px", height: offsetHeight+"px",
-//						position: "absolute", opacity: "0.001", zIndex: 10000
-//					  })
-//					  .css($(this).offset())
-//					  .appendTo("body")
-//					  .data("resizable", { width: offsetWidth, height: offsetHeight });
-//				  });
-//				},
-//				resize: function(event, ui)
-//				{
-//				  var self = $('#simplemodal-container').data("resizable"), o = self.options, os = self.originalSize, op = self.originalPosition;
-//
-//				  var delta = {
-//					height: (self.size.height - os.height) || 0, width: (self.size.width - os.width) || 0,
-//					top: (self.position.top - op.top) || 0, left: (self.position.left - op.left) || 0
-//				  },
-//
-//				  _alsoResize = function(exp, c) {
-//					$(exp).each(function() {
-//					  var el = $(this), start = $(this).data("resizable"), style = {}, css = c && c.length ? c : ['width', 'height', 'top', 'left'];
-//
-//					  $.each(css || ['width', 'height', 'top', 'left'], function(i, prop) {
-//						// iframeより少し大きめに設定することで、iframe上でmouseupしてしまったときにmouseupイベント捕捉できない問題解消
-//						var sum = (start[prop]||0) + (delta[prop]||0) + 5;
-//						if (sum && sum >= 0)
-//						  style[prop] = sum || null;
-//					  });
-//
-//					  //Opera fixing relative position
-//					  if (/relative/.test(el.css('position')) && $.browser.opera) {
-//						self._revertToRelativePosition = true;
-//						el.css({ position: 'absolute', top: 'auto', left: 'auto' });
-//					  }
-//
-//					  el.css(style);
-//					});
-//				  };
-//
-//				  _alsoResize('div.ui-resizable-iframeFix', ['width', 'height']);
-//				},
-//				stop: function(event, ui)
-//				{
-//				  // Remove frame helpers
-//				  $("div.ui-resizable-iframeFix")
-//					.removeData('resizable')
-//					.each(function() { this.parentNode.removeChild(this); });
-//				}
-//			});
-//			setTimeout(function(){ popW.contentWindow.focus(); }, 100);
-//		}(jQuery)
-//	);
-
-//	jQuery('#modalContent').modal({onShow: function (dialog) {
-//		dialog.container.resizable();
-//	}});
-
+	$('#xelf_window').load(
+		function(e){
+			setTimeout(function(){ e.target.contentWindow.focus(); }, 100);
+		}
+	);
 
 	if (returnwindow != null){
-		return popW;
+		return $('#xelf_window');
 	}
-
 }
