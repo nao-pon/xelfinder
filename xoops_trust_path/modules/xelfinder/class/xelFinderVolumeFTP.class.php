2d0a027dc8e19a7656f0ee10971e4e50197caada
nao-pon <hypweb@gmail.com>
2012-04-20 23:51:31 +0900

    class elFinderVolumeFTPx for xelFinder

---
+++ xoops_trust_path/modules/xelfinder/class/xelFinderVolumeFTP.class.php
@@ -0,0 +1,20 @@
+<?php
+require_once _MD_ELFINDER_LIB_PATH . '/php/elFinderVolumeFTP.class.php';
+
+elFinder::$netDrivers['ftp'] = 'FTPx';
+
+class elFinderVolumeFTPx extends elFinderVolumeFTP {
+	
+	protected function configure() {
+		$this->tmbPath = XOOPS_ROOT_PATH . '/modules/'._MD_ELFINDER_MYDIRNAME.'/cache';
+		
+		parent::configure();
+
+		$this->disabled[] = 'pixlr';
+	}
+	
+	protected function doSearch($path, $q, $mimes) {
+		$result = array();
+		return $result;
+	}
+}
