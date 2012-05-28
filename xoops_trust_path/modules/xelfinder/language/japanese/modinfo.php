960807118b02e0a83659725322654876e12c8456
nao-pon <hypweb@gmail.com>
2012-05-28 10:43:24 +0900

    update language files (add "[xelfinder_db]" each xelfinder_db settings)

--- xoops_trust_path/modules/xelfinder/language/japanese/modinfo.php
+++ xoops_trust_path/modules/xelfinder/language/japanese/modinfo.php
@@ -38,58 +38,58 @@ define( $constpref.'_DROPBOX_TOKEN' ,           'Dropbox.com アプリケーション Ke
 define( $constpref.'_DROPBOX_TOKEN_DESC' ,      'Developers - Dropbox [ https://www.dropbox.com/developers ]' );
 define( $constpref.'_DROPBOX_SECKEY' ,          'Dropbox.com アプリケーション Secret key' );
 define( $constpref.'_DROPBOX_SECKEY_DESC' ,     '' );
-define( $constpref.'_THUMBNAIL_SIZE' ,          '画像挿入時のサムネイルサイズ' );
+define( $constpref.'_THUMBNAIL_SIZE' ,          '[xelfinder_db] 画像挿入時のサムネイルサイズ' );
 define( $constpref.'_THUMBNAIL_SIZE_DESC' ,     'BBコードでの画像挿入時のサムネイルサイズの規定値(px)' );
-define( $constpref.'_DEFAULT_ITEM_PERM' ,       '作成されるアイテムのパーミッション' );
+define( $constpref.'_DEFAULT_ITEM_PERM' ,       '[xelfinder_db] 作成されるアイテムのパーミッション' );
 define( $constpref.'_DEFAULT_ITEM_PERM_DESC' ,  'パーミッションは3桁で[ファイルオーナー][グループ][ゲスト]<br />各桁 2進数4bitで [非表示(h)][読み込み(r)][書き込み(w)][ロック解除(u)]<br />744: オーナー 7 = -rwu, グループ 4 = -r--, ゲスト 4 = -r--' );
-define( $constpref.'_USE_USERS_DIR' ,           'ユーザー別ホルダの使用' );
+define( $constpref.'_USE_USERS_DIR' ,           '[xelfinder_db] ユーザー別ホルダの使用' );
 define( $constpref.'_USE_USERS_DIR_DESC' ,      '' );
-define( $constpref.'_USERS_DIR_PERM' ,          'ユーザー別ホルダのパーミッション' );
+define( $constpref.'_USERS_DIR_PERM' ,          '[xelfinder_db] ユーザー別ホルダのパーミッション' );
 define( $constpref.'_USERS_DIR_PERM_DESC' ,     'ここでの設定は作成時のみ参照されます。作成後は elFinder で直接変更してください。<br />例: 7cc: オーナー 7 = -rwu, グループ c = hr--, ゲスト c = hr--' );
-define( $constpref.'_USERS_DIR_ITEM_PERM' ,     'ユーザー別ホルダに作成されるアイテムのパーミッション' );
+define( $constpref.'_USERS_DIR_ITEM_PERM' ,     '[xelfinder_db] ユーザー別ホルダに作成されるアイテムのパーミッション' );
 define( $constpref.'_USERS_DIR_ITEM_PERM_DESC' ,'ここでの設定は作成時のみ参照されます。作成後は elFinder で直接変更してください。<br />例: 7cc: オーナー 7 = -rwu, グループ c = hr--, ゲスト c = hr--' );
-define( $constpref.'_USE_GUEST_DIR' ,           'ゲスト用ホルダの使用' );
+define( $constpref.'_USE_GUEST_DIR' ,           '[xelfinder_db] ゲスト用ホルダの使用' );
 define( $constpref.'_USE_GUEST_DIR_DESC' ,      '' );
-define( $constpref.'_GUEST_DIR_PERM' ,          'ゲスト用ホルダのパーミッション' );
+define( $constpref.'_GUEST_DIR_PERM' ,          '[xelfinder_db] ゲスト用ホルダのパーミッション' );
 define( $constpref.'_GUEST_DIR_PERM_DESC' ,     'ここでの設定は作成時のみ参照されます。作成後は elFinder で直接変更してください。<br />例: 766: オーナー 7 = -rwu, グループ 6 = -rw-, ゲスト 6 = -rw-' );
-define( $constpref.'_GUEST_DIR_ITEM_PERM' ,     'ゲスト用ホルダに作成されるアイテムのパーミッション' );
+define( $constpref.'_GUEST_DIR_ITEM_PERM' ,     '[xelfinder_db] ゲスト用ホルダに作成されるアイテムのパーミッション' );
 define( $constpref.'_GUEST_DIR_ITEM_PERM_DESC' ,'ここでの設定は作成時のみ参照されます。作成後は elFinder で直接変更してください。<br />例: 744: オーナー 7 = -rwu, グループ 4 = -r--, ゲスト 4 = -r--' );
-define( $constpref.'_USE_GROUP_DIR' ,           'グループ別ホルダの使用' );
+define( $constpref.'_USE_GROUP_DIR' ,           '[xelfinder_db] グループ別ホルダの使用' );
 define( $constpref.'_USE_GROUP_DIR_DESC' ,      '' );
-define( $constpref.'_GROUP_DIR_PARENT' ,        'グループ別ホルダの親ホルダ名' );
+define( $constpref.'_GROUP_DIR_PARENT' ,        '[xelfinder_db] グループ別ホルダの親ホルダ名' );
 define( $constpref.'_GROUP_DIR_PARENT_DESC' ,   '' );
-define( $constpref.'_GROUP_DIR_PARENT_NAME' ,   'グループ毎閲覧');
-define( $constpref.'_GROUP_DIR_PERM' ,          'グループ別ホルダのパーミッション' );
+define( $constpref.'_GROUP_DIR_PARENT_NAME' ,   '[xelfinder_db] グループ毎閲覧');
+define( $constpref.'_GROUP_DIR_PERM' ,          '[xelfinder_db] グループ別ホルダのパーミッション' );
 define( $constpref.'_GROUP_DIR_PERM_DESC' ,     'ここでの設定は作成時のみ参照されます。作成後は elFinder で直接変更してください。<br />例: 768: オーナー 7 = -rwu, グループ 6 = -rw-, ゲスト 8 = h---' );
-define( $constpref.'_GROUP_DIR_ITEM_PERM' ,     'グループ別ホルダに作成されるアイテムのパーミッション' );
+define( $constpref.'_GROUP_DIR_ITEM_PERM' ,     '[xelfinder_db] グループ別ホルダに作成されるアイテムのパーミッション' );
 define( $constpref.'_GROUP_DIR_ITEM_PERM_DESC' ,'ここでの設定は作成時のみ参照されます。作成後は elFinder で直接変更してください。<br />例: 748: オーナー 7 = -rwu, グループ 4 = -r--, ゲスト 8 = h---' );
 
-define( $constpref.'_UPLOAD_ALLOW_ADMIN' ,      '管理者にアップロードを許可する MIME タイプ' );
+define( $constpref.'_UPLOAD_ALLOW_ADMIN' ,      '[xelfinder_db] 管理者にアップロードを許可する MIME タイプ' );
 define( $constpref.'_UPLOAD_ALLOW_ADMIN_DESC' , 'MIME タイプを半角スペース区切りで記述。<br />all: 全て許可, none: 何も許可しない<br />例: image text/plain' );
-define( $constpref.'_AUTO_RESIZE_ADMIN' ,       '管理者用自動リサイズ (px)' );
+define( $constpref.'_AUTO_RESIZE_ADMIN' ,       '[xelfinder_db] 管理者用自動リサイズ (px)' );
 define( $constpref.'_AUTO_RESIZE_ADMIN_DESC' ,  '画像をアップロード時、指定した矩形サイズに収まるように自動リサイズする値(px)。<br />何も入力しないと自動リサイズは行われません。' );
 
-define( $constpref.'_SPECIAL_GROUPS' ,          '特定グループ' );
+define( $constpref.'_SPECIAL_GROUPS' ,          '[xelfinder_db] 特定グループ' );
 define( $constpref.'_SPECIAL_GROUPS_DESC' ,     '特定グループとするグループを選択 (複数選択可)' );
-define( $constpref.'_UPLOAD_ALLOW_SPGROUPS' ,   '特定グループにアップロードを許可する MIME タイプ' );
+define( $constpref.'_UPLOAD_ALLOW_SPGROUPS' ,   '[xelfinder_db] 特定グループにアップロードを許可する MIME タイプ' );
 define( $constpref.'_UPLOAD_ALLOW_SPGROUPS_DESC','' );
-define( $constpref.'_AUTO_RESIZE_SPGROUPS' ,    '特定グループ用自動リサイズ (px)' );
+define( $constpref.'_AUTO_RESIZE_SPGROUPS' ,    '[xelfinder_db] 特定グループ用自動リサイズ (px)' );
 define( $constpref.'_AUTO_RESIZE_SPGROUPS_DESC','' );
 
-define( $constpref.'_UPLOAD_ALLOW_USER' ,       '登録ユーザーにアップロードを許可する MIME タイプ' );
+define( $constpref.'_UPLOAD_ALLOW_USER' ,       '[xelfinder_db] 登録ユーザーにアップロードを許可する MIME タイプ' );
 define( $constpref.'_UPLOAD_ALLOW_USER_DESC' ,  '' );
-define( $constpref.'_AUTO_RESIZE_USER' ,        '登録ユーザー用自動リサイズ (px)' );
+define( $constpref.'_AUTO_RESIZE_USER' ,        '[xelfinder_db] 登録ユーザー用自動リサイズ (px)' );
 define( $constpref.'_AUTO_RESIZE_USER_DESC',    '' );
 
-define( $constpref.'_UPLOAD_ALLOW_GUEST' ,      'ゲストにアップロードを許可する MIME タイプ' );
+define( $constpref.'_UPLOAD_ALLOW_GUEST' ,      '[xelfinder_db] ゲストにアップロードを許可する MIME タイプ' );
 define( $constpref.'_UPLOAD_ALLOW_GUEST_DESC' , '' );
-define( $constpref.'_AUTO_RESIZE_GUEST' ,       'ゲスト用自動リサイズ (px)' );
+define( $constpref.'_AUTO_RESIZE_GUEST' ,       '[xelfinder_db] ゲスト用自動リサイズ (px)' );
 define( $constpref.'_AUTO_RESIZE_GUEST_DESC',   '' );
 
-define( $constpref.'_DISABLE_PATHINFO' ,        'ファイル参照URLの PathInfo を無効にする' );
+define( $constpref.'_DISABLE_PATHINFO' ,        '[xelfinder_db] ファイル参照URLの PathInfo を無効にする' );
 define( $constpref.'_DISABLE_PATHINFO_DESC' ,   '' );
 
-define( $constpref.'_EDIT_DISABLE_LINKED' ,     'リンク済みファイルの書き込み禁止' );
+define( $constpref.'_EDIT_DISABLE_LINKED' ,     '[xelfinder_db] リンク済みファイルの書き込み禁止' );
 define( $constpref.'_EDIT_DISABLE_LINKED_DESC' ,'リンク切れや不用意な上書きを防止するためにリンク・参照されたファイルを自動的に書き込み禁止に設定します。' );
 
 define( $constpref.'_SSL_CONNECTOR_URL' ,       'セキュア接続 URL' );
