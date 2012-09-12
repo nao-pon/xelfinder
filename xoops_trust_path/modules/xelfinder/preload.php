a6857823ee3ba1a5e5d8b9cbf6f056ec757b95ad
nao-pon <hypweb@gmail.com>
2012-09-12 13:39:30 +0900

    add overRideImagecategoryList into preload
    XOOPS標準のイメージマネージャに登録された画像がない場合、
    管理画面 > 互換モジュール > イメージ・マネージャー のリンク先を X-elFinder にリダイレクトするようにしました。

--- xoops_trust_path/modules/xelfinder/preload.php
+++ xoops_trust_path/modules/xelfinder/preload.php
@@ -7,15 +7,20 @@ if( ! preg_match( '/^[0-9a-zA-Z_-]+$/' , $mydirname ) ) exit ;
 if( ! class_exists( 'xelfinderPreloadBase' ) ) {
 
 class xelfinderPreloadBase extends XCube_ActionFilter {
+	
 	function preBlockFilter() {
-		$root =& XCube_Root::getSingleton();
-		$root->mDelegateManager->delete('Legacypage.Imagemanager.Access','Legacy_EventFunction::imageManager');
-		$root->mDelegateManager->add('Legacypage.Imagemanager.Access',
+		$this->mRoot->mDelegateManager->delete('Legacypage.Imagemanager.Access','Legacy_EventFunction::imageManager');
+		$this->mRoot->mDelegateManager->add('Legacypage.Imagemanager.Access',
 									array($this, 'overRideDefaultImageManager'),
 									XCUBE_DELEGATE_PRIORITY_FIRST);
+		
 		$this->mRoot->mDelegateManager->add('Legacy_TextFilter.MakeXCodeConvertTable',
 									array($this, 'addXCodeConvertTable'),
 									XCUBE_DELEGATE_PRIORITY_NORMAL - 1);
+		
+		$this->mRoot->mDelegateManager->add('Legacy_ActionFrame.CreateAction',
+									array($this, 'overRideImagecategoryList'),
+									XCUBE_DELEGATE_PRIORITY_FIRST);
 	}
 
 	function overRideDefaultImageManager() {
@@ -51,6 +56,19 @@ class xelfinderPreloadBase extends XCube_ActionFilter {
 		$rep = '<img src="'.XOOPS_URL.'/\\1" alt="" />';
 		$replacements[0][] = $rep;
 		$replacements[1][] = $rep;
+	}
+	
+	function overRideImagecategoryList(& $actionFrame) {
+		if ($actionFrame->mActionName === 'ImagecategoryList') {
+			$image_handler =& xoops_getmodulehandler('image');
+			if ($image_handler) {
+				$total_criteria =new CriteriaCompo(new Criteria('imgcat_id', 0));
+				if (! $image_handler->getCount($total_criteria)) {
+					$root =& XCube_Root::getSingleton();
+					$root->mController->executeForward(XOOPS_MODULE_URL . '/' . $this->mydirname . '/');
+				}
+			}
+		}
 	}
 }
 
