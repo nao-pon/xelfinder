6530a276970d9959176ee6a9ecdafcc867ebfc63
nao-pon <hypweb@gmail.com>
2013-03-23 11:11:09 +0900

    [common] js SimpleModal up to 1.4.4

--- html/common/js/simplemodal/js/basic.js
+++ html/common/js/simplemodal/js/basic.js
@@ -1,14 +1,11 @@
 /*
  * SimpleModal Basic Modal Dialog
- * http://www.ericmmartin.com/projects/simplemodal/
- * http://code.google.com/p/simplemodal/
+ * http://simplemodal.com
  *
- * Copyright (c) 2010 Eric Martin - http://ericmmartin.com
+ * Copyright (c) 2013 Eric Martin - http://ericmmartin.com
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
- *
- * Revision: $Id: basic.js 254 2010-07-23 05:14:44Z emartin24 $
  */
 
 jQuery(function ($) {
