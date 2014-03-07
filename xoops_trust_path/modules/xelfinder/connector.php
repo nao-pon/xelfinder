52ff94111be7a27710348533326ce848edb2de60
nao-pon <hypweb@gmail.com>
2014-03-07 15:51:33 +0900

    fix ORIGN check is opposite

--- xoops_trust_path/modules/xelfinder/connector.php
+++ xoops_trust_path/modules/xelfinder/connector.php
@@ -28,7 +28,7 @@ $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
 if (! isset($_SESSION['XELFINDER_CTOKEN'])
 		|| ! isset($_REQUEST['ctoken'])
 		|| $_SESSION['XELFINDER_CTOKEN'] !== $_REQUEST['ctoken']) {
-	! $origin || (isset($_REQUEST['cmd']) && $_REQUEST['cmd'] === 'file') || exit(json_encode(array('error' => 'errAccess')));
+	$origin || (isset($_REQUEST['cmd']) && $_REQUEST['cmd'] === 'file') || exit(json_encode(array('error' => 'errAccess')));
 }
 
 define('_MD_ELFINDER_LIB_PATH', XOOPS_TRUST_PATH . '/libs/elfinder');
