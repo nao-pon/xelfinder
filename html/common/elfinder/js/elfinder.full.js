13f713199435baf6e5b8b4d9b0c3fe9ce4456d25
nao-pon <hypweb@gmail.com>
2014-04-26 09:17:42 +0900

    elFinder up to newly ( nao-pon/elFinder@9964155)

--- html/common/elfinder/js/elfinder.full.js
+++ html/common/elfinder/js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1_n (Nightly: 3c32c2a) (2014-04-24)
+ * Version 2.1_n (Nightly: 9964155) (2014-04-26)
  * http://elfinder.org
  * 
  * Copyright 2009-2013, Studio 42
@@ -2184,12 +2184,26 @@ elFinder.prototype = {
 				return dfrd.promise();
 			} else {
 				var ret = [];
+				var check = [];
 				var str = data.files[0];
 				if (data.type == 'html') {
 					var tmp = $("<html/>").append($.parseHTML(str));
 					$('img[src]', tmp).each(function(){
-						var url = $(this).attr('src');
-						if (url && $.inArray(url, ret) == -1) ret.push(url);
+						var url, purl,
+						self = $(this),
+						pa = self.closest('a');
+						if (pa && pa.attr('href') && pa.attr('href').match(/\.(?:jpe?g|gif|bmp|png)/i)) {
+							purl = pa.attr('href');
+						}
+						url = self.attr('src');
+						if (url) {
+							if (purl) {
+								$.inArray(purl, ret) == -1 && ret.push(purl);
+								$.inArray(url, check) == -1 &&  check.push(url);
+							} else {
+								$.inArray(url, ret) == -1 && ret.push(url);
+							}
+						}
 					});
 					$('a[href]', tmp).each(function(){
 						var loc,
@@ -2201,7 +2215,7 @@ elFinder.prototype = {
 						if ($(this).text()) {
 							loc = parseUrl($(this).attr('href'));
 							if (loc.href && ! loc.pathname.match(/(?:\.html?|\/[^\/.]*)$/i)) {
-								if ($.inArray(loc.href, ret) == -1) ret.push(loc.href);
+								if ($.inArray(loc.href, ret) == -1 && $.inArray(loc.href, check) == -1) ret.push(loc.href);
 							}
 						}
 					});
@@ -3456,7 +3470,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1_n (Nightly: 3c32c2a)';
+elFinder.prototype.version = '2.1_n (Nightly: 9964155)';
 
 
 
