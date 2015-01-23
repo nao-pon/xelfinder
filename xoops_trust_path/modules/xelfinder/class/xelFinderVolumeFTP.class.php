e8ce608476201ee70dc380e1aa7ff1d1eff0656a
nao-pon <hypweb@gmail.com>
2015-01-23 21:34:28 +0900

    [volume:FTPx] enable thumbnail

--- xoops_trust_path/modules/xelfinder/class/xelFinderVolumeFTP.class.php
+++ xoops_trust_path/modules/xelfinder/class/xelFinderVolumeFTP.class.php
@@ -4,14 +4,15 @@ require_once _MD_ELFINDER_LIB_PATH . '/php/elFinderVolumeFTP.class.php';
 elFinder::$netDrivers['ftp'] = 'FTPx';
 
 class elFinderVolumeFTPx extends elFinderVolumeFTP {
+	protected function init() {
+		$this->options['tmpPath'] = XOOPS_MODULE_PATH.'/'._MD_ELFINDER_MYDIRNAME.'/cache';
+		$this->options['tmbPath'] = XOOPS_MODULE_PATH.'/'._MD_ELFINDER_MYDIRNAME.'/cache/tmb';
+		$this->options['tmbURL']  = _MD_XELFINDER_MODULE_URL.'/'._MD_ELFINDER_MYDIRNAME.'/cache/tmb';
+		return parent::init();
+	}
 	
 	protected function configure() {
-		$this->options['tmpPath'] = XOOPS_MODULE_PATH.'/'._MD_ELFINDER_MYDIRNAME.'/cache';
-		
 		parent::configure();
-		
-		$this->tmbURL = '';
-		$this->tmbPath = '';
 		
 		$this->disabled[] = 'pixlr';
 	}
