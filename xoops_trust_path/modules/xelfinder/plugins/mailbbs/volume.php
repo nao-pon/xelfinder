067c6d72c18d9618531b101e56d4eb0a161b2cbf
nao-pon <hypweb@gmail.com>
2015-01-29 17:49:47 +0900

    [plugin:mailbbs] clean up with newly elFinder

--- xoops_trust_path/modules/xelfinder/plugins/mailbbs/volume.php
+++ xoops_trust_path/modules/xelfinder/plugins/mailbbs/volume.php
@@ -13,16 +13,7 @@ if (is_dir(XOOPS_ROOT_PATH . $path)) {
 		'URL'        => _MD_XELFINDER_SITEURL . $path,
 		'alias'      => $title,
 		'icon'       => is_file(XOOPS_MODULE_PATH . '/'.$mydirname.'/imgs/elfinder_volume_icon.png')? _MD_XELFINDER_MODULE_URL . '/'.$mydirname.'/imgs/elfinder_volume_icon.png' : '',
-		'defaults' => array('read' => true, 'write' => false),
-		'attributes' => array(
-			array(
-				'pattern' => '#.+#',
-				'read'    => true,
-				'write'   => false,
-				'locked'  => true,
-				'hidden'  => false
-			)
-		)
+		'defaults' => array('read' => true, 'write' => false, 'locked' => true)
 	);
 
 }
