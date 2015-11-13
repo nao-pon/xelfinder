23ec85be17b1f5cec6b1fd377f228c18ae8b75df
nao-pon <hypweb@gmail.com>
2015-11-13 23:42:52 +0900

    elFinder up to newly ( nao-pon/elFinder@07c9502 )

--- html/common/elfinder/js/elFinder.version.js
+++ html/common/elfinder/js/elFinder.version.js
@@ -3,5 +3,5 @@
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.1 (2.1_n Nightly: f010523)';
+elFinder.prototype.version = '2.1.1 (2.1_n Nightly: 07c9502)';
 
--- html/common/elfinder/js/elfinder.full.js
+++ html/common/elfinder/js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.1 (2.1_n Nightly: f010523) (2015-11-13)
+ * Version 2.1.1 (2.1_n Nightly: 07c9502) (2015-11-13)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -4137,7 +4137,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1.1 (2.1_n Nightly: f010523)';
+elFinder.prototype.version = '2.1.1 (2.1_n Nightly: 07c9502)';
 
 
 
