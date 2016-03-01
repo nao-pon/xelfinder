574058fc6cab71d9118c437cca89a884b5162037
nao-pon <hypweb@gmail.com>
2016-03-01 21:57:47 +0900

    [admin:googledrive] optimization of view and bug fixes

--- xoops_trust_path/modules/xelfinder/admin/googledrive.php
+++ xoops_trust_path/modules/xelfinder/admin/googledrive.php
@@ -3,12 +3,12 @@
 $php54up = false;
 
 if ($php54up = version_compare(PHP_VERSION, '5.4.0', '>=')) {
-
+error_reporting(-1);
 	require $mytrustdirpath . '/plugins/vendor/autoload.php';
 
 	$selfURL = XOOPS_MODULE_URL . '/' . $mydirname . '/admin/index.php?page=googledrive';
 	$sessTokenKey = $mydirname . 'AdminGoogledriveToken';
-	$sessClientKey = $mydirname . 'AdminGoogledriveToken';
+	$sessClientKey = $mydirname . 'AdminGoogledriveClientKey';
 	$client = null;
 	$clientId = $clientSecret = '';
 	
@@ -17,99 +17,105 @@ if ($php54up = version_compare(PHP_VERSION, '5.4.0', '>=')) {
 		$clientSecret = $_SESSION[$sessClientKey]['ClientSecret'];
 	}
 
-	if (! empty ( $_POST['ClientId'] ) && ! empty ( $_POST['ClientSecret'] )) {
-		$clientId = trim ( $_POST['ClientId'] );
-		$clientSecret = trim ( $_POST['ClientSecret'] );
+	if (! empty($_POST['json'])) {
+		$json = @json_decode($_POST['json'], true);
+		if ($json && isset($json['web'])) {
+			$clientId = @$json['web']['client_id'];
+			$clientSecret = @$json['web']['client_secret'];
+		}
+	}
+	if (! empty($_POST['ClientId']) && ! empty($_POST['ClientSecret'])) {
+		$clientId = trim($_POST['ClientId']);
+		$clientSecret = trim($_POST['ClientSecret']);
 		$_SESSION[$sessClientKey] = array (
-				'ClientId' => $clientId,
-				'ClientSecret' => $clientSecret 
+			'ClientId' => $clientId,
+			'ClientSecret' => $clientSecret 
 		);
 	}
 
-	if (! empty ( $_SESSION[$sessClientKey] ) && ! isset ( $_GET ['start'] )) {
+	if (! empty($_SESSION[$sessClientKey]) && !isset($_GET ['start'])) {
 		
-		$client = new \Google_Client ();
-		// クライアントID
-		// $client->setClientId('439932857153-o4vqqv0c9b6cqnbamec2a0h80o9oqkbd.apps.googleusercontent.com');
-		$client->setClientId ( $_SESSION[$sessClientKey] ['ClientId'] );
-		// クライアントSecret ID
-		// $client->setClientSecret('35TbJlEANKrKI7dMVwh7t5xP');
-		$client->setClientSecret ( $_SESSION[$sessClientKey] ['ClientSecret'] );
-		// リダイレクトURL
-		$client->setRedirectUri ( $selfURL );
+		$client = new \Google_Client();
+		$client->setClientId($_SESSION[$sessClientKey]['ClientId']);
+		$client->setClientSecret($_SESSION[$sessClientKey]['ClientSecret']);
+		$client->setRedirectUri($selfURL);
 		
-		$service = new \Google_Service_Drive ( $client );
-		// 許可されてリダイレクトされると URL に code が付加されている
-		// code があったら受け取って、認証する
-		if (isset ( $_GET ['code'] )) {
-			// 認証
-			$client->authenticate ( $_GET ['code'] );
-			$_SESSION[$sessTokenKey] = $client->getAccessToken ();
-			// header('Location: ' . $selfURL);
-			// exit;
+		$service = new \Google_Service_Drive($client);
+		if (isset($_GET['code'])) {
+			$client->authenticate($_GET['code']);
+			$_SESSION[$sessTokenKey] = $client->getAccessToken();
 		}
 		
-		// セッションからアクセストークンを取得
-		if (isset ( $_SESSION[$sessTokenKey] )) {
-			// トークンセット
-			$client->setAccessToken ( $_SESSION[$sessTokenKey] );
+		if (isset($_SESSION[$sessTokenKey]) && isset($_SESSION[$sessTokenKey]['access_token'])) {
+			$client->setAccessToken($_SESSION[$sessTokenKey]);
 		}
 	}
 }
 
-xoops_cp_header ();
-include dirname ( __FILE__ ) . '/mymenu.php';
+xoops_cp_header();
+include dirname(__FILE__) . '/mymenu.php';
 
-echo '<h3>' . xelfinderAdminLang ( 'GOOGLEDRIVE_GET_TOKEN' ) . '</h3>';
+echo '<h3>' . xelfinderAdminLang('GOOGLEDRIVE_GET_TOKEN') . '</h3>';
 
 if ($php54up) {
 	$form = true;
-	// トークンがセットされていたら
 	if ($client) {
-		if (empty ( $_POST ) && $client->getAccessToken ()) {
+		if (empty($_POST) && $client->getAccessToken()) {
 			try {
-				$aToken = $client->getAccessToken ();
+				$aToken = $client->getAccessToken();
 				$token = array (
-						'client_id' => $client->getClientId (),
-						'client_secret' => $client->getClientSecret (),
-						'access_token' => $aToken ['access_token'] 
+					'client_id' => $client->getClientId(),
+					'client_secret' => $client->getClientSecret(),
+					'access_token' => $aToken['access_token']
 				);
-				if (isset ( $aToken ['refresh_token'] )) {
-					unset ( $token ['access_token'] );
-					$token ['refresh_token'] = $aToken ['refresh_token'];
+				if (isset($aToken['refresh_token'])) {
+					unset($token['access_token']);
+					$token['refresh_token'] = $aToken['refresh_token'];
 				}
-				$ext_token = json_encode ( $token );
+				$ext_token = json_encode($token);
 				echo '<h3>Google Drive API Token</h3>';
-				echo '<div><textarea style="width:70%;height:5em;">' . $ext_token . '</textarea></div>';
+				echo '<div><textarea class="allselect" style="width:70%;height:5em;" spellcheck="false">' . $ext_token . '</textarea></div>';
 				echo '<h3>Example to Volume Driver Setting</h3>';
-				echo '<div><textarea style="width:70%;height:7em;">xelfinder:flyGoogleDrive:/[TargetPath]:GoogleDrive:gid=1|id=gd|ext_token=' . $ext_token . '</textarea></div>';
+				echo '<div><p>Folder ID as root: <input type=text id="xelfinder_googledrive_folder" value="root"></input> "root" is <a href="https://drive.google.com/drive/my-drive" target="_blank">"My Drive" of your Google Drive</a>.</p>';
+				echo '<p>You can find the folder ID to the URL(folders/[Folder ID]) of the site of <a href="https://drive.google.com/drive/">GoogleDrive</a>.</p></div>';
+				echo '<div><textarea class="allselect" style="width:70%;height:7em;" id="xelfinder_googledrive_volconf" spellcheck="false">xelfinder:flyGoogleDrive:root:GoogleDrive:gid=1|id=gd|ext_token=' . $ext_token . '</textarea></div>';
+				echo "<script>(function($){
+					$('#xelfinder_googledrive_folder').on('change keyup mouseup paste', function(e) {
+						var self = $(this);
+						setTimeout(function(){
+							var conf = $('#xelfinder_googledrive_volconf');
+								data = conf.val();
+							conf.val(data.replace(/flyGoogleDrive:[^:]*:/, 'flyGoogleDrive:' + self.val() + ':'));
+						}, e.type === 'paste'? 100 : 0);
+					});
+					$('textarea.allselect').on('focus', function() { $(this).select(); });
+				})(jQuery);</script>";
 				echo '<h4><a href="' . $selfURL . '&start">Reauthorization</a></h4>';
 				$form = false;
-			} catch ( Google_Exception $e ) {
-				echo $e->getMessage ();
+			} catch(Google_Exception $e) {
+				echo $e->getMessage();
 			}
-		} else if (! empty ( $_POST['scopes'] )) {
-			if (! empty ( $_POST['revoke'] )) {
-				$client->revokeToken ();
+		} elseif (! empty($_POST['scopes'])) {
+			if (! empty($_POST['revoke'])) {
+				$client->revokeToken();
 			}
-			// 認証用URL取得
-			$scopes = array ();
-			foreach ( $_POST['scopes'] as $scope ) {
+			$scopes = array();
+			foreach($_POST['scopes'] as $scope) {
 				switch ($scope) {
 					case 'DRIVE' :
 					case 'DRIVE_READONLY' :
 					case 'DRIVE_FILE' :
 					case 'DRIVE_PHOTOS_READONLY' :
 					case 'DRIVE_APPS_READONLY' :
-						$scopes [] = constant ( 'Google_Service_Drive::' . $scope );
+						$scopes[] = constant('Google_Service_Drive::' . $scope);
 				}
 			}
-			$client->setScopes ( $scopes );
-			if (! empty ( $_POST['offline'] )) {
-				$client->setApprovalPrompt ( 'force' );
-				$client->setAccessType ( 'offline' );
+			$client->setScopes($scopes);
+			if (! empty($_POST['offline'])) {
+				$client->setApprovalPrompt('force');
+				$client->setAccessType('offline');
 			}
-			$authUrl = $client->createAuthUrl ();
+			$authUrl = $client->createAuthUrl();
 			echo '<a href="' . $authUrl . '">Please allow the application access.</a>';
 			$form = false;
 		}
@@ -118,9 +124,22 @@ if ($php54up) {
 ?>
 <h3>Authentication options</h3>
 <hr>
-<p>Redirect URI: <?php echo $selfURL; ?></p>
+<h4>Step by step</h4>
+<ol>
+	<li>Make Project in Google Developers Console (<a href="https://console.developers.google.com/project" target="_brank">https://console.developers.google.com/project</a>)</li>
+	<li>Enable Drive API</li>
+	<li>Make Authentication  infomation (Type of Web Server & User data)</li>
+	<li>Make OAuth 2.0 Client<br />(Redirect URI: <?php echo $selfURL; ?> )</li>
+	<li>Get JSON and Paste it next TextArea</li>
+	<li>And Click "Get authentication link"</li>
+	<li>Then Approve this app in your account</li>
+</ol>
 <form action="index.php?page=googledrive" method="post">
 	<h4>Web client ID and Secret key</h4>
+	<p>
+		JSON: <textarea name="json"></textarea>
+	</p>
+	<p><strong>OR</strong></p>
 	<p>
 		ClientId: <input type="text" name="ClientId" style="width: 50em"
 			value="<?php echo htmlspecialchars($clientId); ?>"><br />
@@ -133,13 +152,13 @@ if ($php54up) {
 	<h4>Required scopes</h4>
 	<p>
 		<input type="checkbox" name="scopes[]" value="DRIVE" checked="checked">:
-		Google Drive ( Read & Write )<br> <input type="checkbox"
-			name="scopes[]" value="DRIVE_READONLY">: Google Drive ( Read only )<br>
+		Google Drive(Read & Write)<br> <input type="checkbox"
+			name="scopes[]" value="DRIVE_READONLY">: Google Drive(Read only)<br>
 		<input type="checkbox" name="scopes[]" value="DRIVE_FILE">: Google
-		Drive ( Read & Write, Only opened or created with this app)<br> <input
+		Drive(Read & Write, Only opened or created with this app)<br> <input
 			type="checkbox" name="scopes[]" value="DRIVE_PHOTOS_READONLY">:
-		Google Photes ( Read only )<br> <input type="checkbox" name="scopes[]"
-			value="DRIVE_APPS_READONLY">: Google Appss ( Read only )
+		Google Photes(Read only)<br> <input type="checkbox" name="scopes[]"
+			value="DRIVE_APPS_READONLY">: Google Appss(Read only)
 	</p>
 	<h4>Continuous connectivity</h4>
 	<div>Are you need refresh token?</div>
@@ -160,4 +179,4 @@ if ($php54up) {
 <p>GoogleDrive Driver Require PHP >= 5.4 . Your PHP version is <?php echo PHP_VERSION; ?> .</p>
 <?php
 }
-xoops_cp_footer ();
+xoops_cp_footer();
